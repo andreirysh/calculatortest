2013bf8a658b62da38f89b5d516e0223d39f649e
andreirysh <andreirysh@gmail.com>
2024-11-14 15:47:36 +0100

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+export default {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    rules: {
+        'no-console': 'warn',
+        'no-eval': 'error',
+    },
+};
