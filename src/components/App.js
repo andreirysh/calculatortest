8b11a24e41636b9c3282780a1495fc8042aa75d5
andreirysh <andreirysh@gmail.com>
2024-11-14 15:29:06 +0100

    theme toggle added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
-import Calculator from './Calculator.js'
+import Calculator from './Calculator.js';
 
 export default function App() {
     const screen = document.getElementById('screen');
     const calculator = new Calculator(screen);
     const buttons = document.querySelectorAll('.button');
+
+    const themeToggleButton = document.getElementById('theme-toggle');
+    const calculatorContainer = document.querySelector('.calculator');
+
+    if (localStorage.getItem('theme') === 'dark') {
+        calculatorContainer.classList.add('dark');
+        themeToggleButton.style.backgroundColor = '#535353';
+    } else {
+        calculatorContainer.classList.add('light');
+        themeToggleButton.style.backgroundColor = '#fff';
+    }
+
+    themeToggleButton.addEventListener('click', () => {
+        calculatorContainer.classList.toggle('dark');
+        calculatorContainer.classList.toggle('light');
+
+        if (calculatorContainer.classList.contains('dark')) {
+            localStorage.setItem('theme', 'dark');
+            themeToggleButton.style.backgroundColor = '#535353';
+        } else {
+            localStorage.setItem('theme', 'light');
+            themeToggleButton.style.backgroundColor = '#fff';
+        }
+    });
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
